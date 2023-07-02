3601d2af63ef293ab9132fcf28cf2cf4bd0820ff
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-07-02 16:51:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,3 +20,8 @@ function getFormvalue() {
   // Prevent form submission
   return false;
 }
+
+// const Formele=document.getElementById("form1");
+// 	const name=Formele.elements['fname'].value;
+// 	const lname=Formele.elements['lname'].value;
+// 	alert(`${name} ${lname}`);
