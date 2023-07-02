f4376346d0a2a050b13571ac80fae2f05b8c2739
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-07-02 16:57:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-// function getFormvalue() {
-//     //Write your code here
-// function getFormvalue() {
-//   // Get the form element
-//   var form = document.getElementById("form1");
+ //Write your code here
+function getFormvalue() {
+  // Get the form element
+  var form = document.getElementById("form1");
 
-//   // Get the values of the first and last name input fields
-//   var firstName = form.fname.value;
-//   var lastName = form.lname.value;
+  // Get the values of the first and last name input fields
+  var firstName = form.fname.value;
+  var lastName = form.lname.value;
 
-//   // Display the first and last name using alert()
-//   alert("First Name: " + firstName + "\nLast Name: " + lastName);
+  // Display the first and last name using alert()
+  alert("First Name: " + firstName + "Last Name: " + lastName);
 
-//   // Prevent form submission
-//   return false;
-// }
+  // Prevent form submission
+  return false;
+}
 
-const Formele=document.getElementById("form1");
-	const name=Formele.elements['fname'].value;
-	const lname=Formele.elements['lname'].value;
-	alert(`${name} ${lname}`);
+// const Formele=document.getElementById("form1");
+// 	const name=Formele.elements['fname'].value;
+// 	const lname=Formele.elements['lname'].value;
+// 	alert(`${name} ${lname}`);
